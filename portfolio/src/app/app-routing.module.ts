2e9c6657c53513da6367538122c89a60704a4e1c
Dos Santos Davide <dos.davide91@gmail.com>
2022-08-12 10:39:50 +0200

    Commit
    Page not found

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjetComponent } from './pages/projet/projet.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'skills', component: SkillsComponent},
   {path: 'projet', component: ProjetComponent},
   {path: 'contact',component:ContactComponent},
+  { path: '**', component: PageNotFoundComponent } // Route pour erreur 404, toujours décalré en dernier
 ];
 
 @NgModule({
--- portfolio/src/app/page-not-found/page-not-found.component.ts
+++ portfolio/src/app/page-not-found/page-not-found.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  template: ` `,
+  template: `<h1>Page not found</h1> `,
 
 
 })
