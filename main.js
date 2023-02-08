4655257afb528f5bd19c5a932d77a244cbe58e87
Dos Santos Davide <dos.davide91@gmail.com>
2023-02-08 17:32:08 +0100

    Commit Modal JS and CSS

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+const projets = document.querySelectorAll(`.listprojets article`);
+const modal = document.getElementById(`myModal`);
+const modalImg =document.getElementById(`modal-content`);
+const captionText = document.getElementById(`caption`);
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("imgProjet");
-var modalImg = document.getElementById("projet");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+projets.forEach(projet =>{
+  const img =projet.querySelector(`.imgProjet`)
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    document.getElementById(`navbar`).style.display = "none"
+    document.body.style.overflow = "hidden";
+  }
+})
+
+
+
+
+
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  document.body.style.overflow = "auto";
+  document.getElementById(`navbar`).style.display = "flex"
+
 }
 
 
