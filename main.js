7aaf9fafa862d7b73abb33c49abb501b66ccc395
Dos Santos Davide <dos.davide91@gmail.com>
2023-02-28 21:42:14 +0100

    commit
    Ajout fonctionalité dark/light mode

--- main.js
+++ main.js
@@ -34,6 +34,29 @@
 })(jQuery)
 
 
+// Fonction pour dark/light mode
+
+const btnToggle = document.querySelector(".btn-toggle");
+btnToggle.addEventListener("click", () =>{
+
+    const body = document.body;
+    if (body.classList.contains("dark")){
+      body.classList.add("light")
+      body.classList.remove("dark")
+      btnToggle.innerHTML = "Go White"
+
+    }else if(body.classList.contains("light")){
+      body.classList.add("dark")
+      body.classList.remove("light")
+      btnToggle.innerHTML = "Go Dark"
+    }
+
+
+})
+
+
+
+
 
 const menuburger = document.querySelector(".menuburger");
 const navbar = document.getElementById("navbar");
